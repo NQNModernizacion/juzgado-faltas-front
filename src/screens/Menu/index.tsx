6645dbd738f0d01da87c421d79f60a713ec223c1
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2026-06-05 14:27:18 -0300

    Update index.tsx

--- src/screens/Menu/index.tsx
+++ src/screens/Menu/index.tsx
@@ -6,12 +6,15 @@ const Menu = () => {
 
   return (
     <Container title="Menú Principal">
-      <div className="m-auto max-w-64 space-y-3">
+      <div className="m-auto max-w-64 space-y-3 ">
         <ButtonBase className="w-full" onClick={() => nav('/acta/alta')}>
           Alta de actas
         </ButtonBase>
         <ButtonBase className="w-full" onClick={() => nav('/acta/listado')}>
           Listado de actas
+        </ButtonBase>
+        <ButtonBase className="w-full" onClick={() => nav('/grupos')}>
+          Grupos
         </ButtonBase>
         <ButtonBase className="w-full" onClick={() => nav('_viewcom')}>
           Componentes
