9ace4862853d0466316f5b09b4b46c69082d9116
Leo Apablaza <leo.apablaza53@gmail.com>
2026-07-06 13:08:21 -0300

    nuevos colores para expediente

---
+++ src/config/actaOptions.ts
@@ -0,0 +1,10 @@
+export const COLOR_OPTIONS: { value: string; label: string }[] = [
+  { value: '#F48FB1', label: 'Rosa' },
+  { value: '#9E9E9E', label: 'Gris' },
+  { value: '#E53935', label: 'Rojo' },
+  { value: '#1E88E5', label: 'Azul' },
+  { value: '#43A047', label: 'Verde' },
+  { value: '#FDD835', label: 'Amarillo' },
+  { value: '#8E24AA', label: 'Morado' },
+  { value: '#FB8C00', label: 'Naranja' },
+]
